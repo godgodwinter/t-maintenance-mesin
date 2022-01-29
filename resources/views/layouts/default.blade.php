0af8bc8eda9e31c3797120b06995bc8d83c0d4f1
godgodwinter <kakadz.pro@gmail.com>
2022-01-29 16:43:53 +0700

    update header

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -23,6 +23,7 @@
           <ul class="navbar-nav mr-3">
             <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
             {{-- <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li> --}}
+
           </ul>
         </form>
 
@@ -37,11 +38,10 @@
 
         </div>
         </div> --}}
-
         <ul class="navbar-nav navbar-right">
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user text-monza">
             <img alt="image" src="{{ asset('/') }}assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-            <div class="d-sm-none d-lg-inline-block">{{ Auth::user()->name }}</div></a>
+            <div class="d-sm-none d-lg-inline-block">{{ Auth::user()->name }} ( {{ ucfirst(Auth::user()->tipeuser) }} )</div></a>
             <div class="dropdown-menu dropdown-menu-right">
               <a href="{{url('/')}}" class="dropdown-item has-icon">
                 <i class="fas fa-house-damage"></i> Beranda Luar
